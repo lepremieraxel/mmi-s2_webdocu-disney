6de284be7ff90d0cd6c256b163690e9984894be0
lepremieraxel <axelmarcial151003@gmail.com>
2022-04-26 22:33:25 +0200

    v7

---
+++ forum/forum.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+    require_once '../config/config.php';
+
+    if(isset($_POST['message']) && !empty($_POST['message']))
+    {
+        $message = htmlspecialchars($_POST['message']);
+
+        $check = $bdd->prepare('SELECT token, pseudo FROM users WHERE token = ?');
+        $check->execute(array($_SESSION['user']));
+        $data = $check->fetch();
+
+        if(strlen($message) <= 350)
+        {
+            $insert = $bdd->prepare('INSERT INTO forum(pseudo, message, token_user, token) VALUES(:pseudo, :message, :token_user, :token)');
+            $insert->execute(array(
+                'pseudo' => $data['pseudo'],
+                'message' => $message,
+                'token_user' => $data['token'],
+                'token' => bin2hex(openssl_random_pseudo_bytes(64)
+                )
+            ));
+            header('Location:index.php'); die();
+        }else header('Location:index.php?forum_err=length'); die();
+    }else header('Location:index.php?forum_err=empty'); die();
