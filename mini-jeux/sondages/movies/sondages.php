5b6100f6bf34cc62fcbaa8fe7272aaca4b95882c
lepremieraxel <axelmarcial151003@gmail.com>
2022-04-28 00:43:40 +0200

    sondages

--- mini-jeux/sondages/movies/sondages.php
+++ mini-jeux/sondages/movies/sondages.php
@@ -14,14 +14,14 @@ session_start();
         $s8 = htmlspecialchars($_POST['s8']);
         $s9 = htmlspecialchars($_POST['s9']);
 
-        $check = $bdd->prepare('SELECT * FROM s_perso WHERE token_user = ?');
+        $check = $bdd->prepare('SELECT * FROM s_films WHERE token_user = ?');
         $check->execute(array($_SESSION['user']));
         $data = $check->fetch();
         $row = $check->rowCount();
 
         if($row == 0)
         {
-            $insert = $bdd->prepare('INSERT INTO s_perso(token_user, token, s1, s2, s3, s4, s5, s6, s7, s8, s9) VALUES(:token_user, :token, :s1, :s2, :s3, :s4, :s5, :s6, :s7, :s8, :s9)');
+            $insert = $bdd->prepare('INSERT INTO s_films(token_user, token, s1, s2, s3, s4, s5, s6, s7, s8, s9) VALUES(:token_user, :token, :s1, :s2, :s3, :s4, :s5, :s6, :s7, :s8, :s9)');
             $insert->execute(array(
                 'token_user' => $_SESSION['user'],
                 'token' => bin2hex(openssl_random_pseudo_bytes(64)),
@@ -37,7 +37,7 @@ session_start();
             ));
             header('Location:index.php?sondage_err=success'); die();
         }else{
-            $update = $bdd->prepare('UPDATE s_perso SET s1 = ?, s2 = ?, s3 = ?, s4 = ?, s5 = ?, s6 = ?, s7 = ?, s8 = ?, s9 = ? WHERE token_user = ?');
+            $update = $bdd->prepare('UPDATE s_films SET s1 = ?, s2 = ?, s3 = ?, s4 = ?, s5 = ?, s6 = ?, s7 = ?, s8 = ?, s9 = ? WHERE token_user = ?');
             $update->execute(array($s1, $s2, $s3, $s4, $s5, $s6, $s7, $s8, $s9, $_SESSION['user']));
             header('Location:index.php?sondage_err=update'); die();
         }
